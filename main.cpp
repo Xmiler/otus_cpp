a236f8aa14098720d5c8ff3aa0812f2ae0cd8eae
Alexander Uldin <alex.uldin@gmail.com>
2020-11-10 22:41:34 +0300

    add trivial allocator

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <map>
 
+template <typename T>
+class ArenaAllocator {
+public:
+    using value_type = T;
+
+    ArenaAllocator() = default;
+
+    template <typename U>
+    explicit ArenaAllocator<T>(const ArenaAllocator<U> &) {
+    }
+
+    T* allocate(std::size_t n) {
+
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+        auto p = std::malloc(n * sizeof(T));
+        if (p == nullptr)
+            throw std::bad_alloc();
+        return static_cast<T *>(p);
+    }
+
+    void deallocate(T* p, std::size_t) {
+
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+        std::free(static_cast<void *>(p));
+    }
+};
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+
+    std::map<int, int, std::less<>, ArenaAllocator<int>> m;
+
+    m[0] = 1;
+    for ( int i = 1; i < 10; ++i )
+        m[i] = m[i-1] * i;
+
     return 0;
 }
