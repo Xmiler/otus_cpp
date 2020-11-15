45fb81fcd55f04c104bbdc0f647eab47f5fbf7e8
Alexander Uldin <alex.uldin@gmail.com>
2020-11-15 11:46:48 +0300

    first version of allocator

--- main.cpp
+++ main.cpp
@@ -1,42 +1,70 @@
 #include <iostream>
 #include <map>
 
-template <typename T>
+template <typename T, size_t N>
 class ArenaAllocator {
 public:
     using value_type = T;
+    static const size_t arena_size = N;
 
-    ArenaAllocator() = default;
+    explicit ArenaAllocator() {
+        arena_ptr = static_cast<T*>(std::malloc(arena_size*sizeof(T)));
+        if ( arena_ptr == nullptr )
+            throw std::bad_alloc();
+    };
 
-    template <typename U>
-    explicit ArenaAllocator<T>(const ArenaAllocator<U> &) {
+    ~ArenaAllocator() {
+        std::free(static_cast<void*>(arena_ptr));
     }
+
+    template <typename T2, size_t N2>
+    explicit ArenaAllocator(const ArenaAllocator<T2, N2> &) {
+    }
+
+    template <typename T2>
+    struct rebind {
+        using other = ArenaAllocator<T2, N>;
+    };
 
     T* allocate(std::size_t n) {
 
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        arena_position++;
+        if ( arena_position >= N )
+            throw std::runtime_error("memory limit is exceeded");
 
-        auto p = std::malloc(n * sizeof(T));
-        if (p == nullptr)
-            throw std::bad_alloc();
-        return static_cast<T *>(p);
+        return &arena_ptr[arena_position];
     }
 
     void deallocate(T* p, std::size_t) {
-
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
-
-        std::free(static_cast<void *>(p));
     }
+
+private:
+    T* arena_ptr = nullptr;
+    int arena_position = -1;
 };
 
-int main() {
-
-    std::map<int, int, std::less<>, ArenaAllocator<int>> m;
-
+template <typename T>
+void fill(T &m) {
     m[0] = 1;
     for ( int i = 1; i < 10; ++i )
         m[i] = m[i-1] * i;
+}
+
+template <typename T>
+void print(const T &m) {
+    for (auto [k, v] : m) {
+        std::cout << k << " " << v << std::endl;
+    }
+}
+
+int main() {
+
+    std::map<int, int, std::less<>> m1;
+    fill(m1);
+
+    std::map<int, int, std::less<>, ArenaAllocator<std::pair<const int, int>, 10>> m2;
+    fill(m2);
+    print(m2);
 
     return 0;
 }
