6055eef84232916d78f47f69afc219760d15cb2f
Alexander Uldin <alex.uldin@gmail.com>
2020-12-11 11:21:21 +0300

    make working with vector and list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <tuple>
 #include <iostream>
 #include <type_traits>
 #include <vector>
+#include <list>
+
+template <typename T>
+struct is_vector_or_list : std::false_type {};
+
+template <typename T>
+struct is_vector_or_list<std::vector<T>> : std::true_type {};
+
+template <typename T>
+struct is_vector_or_list<std::list<T>> : std::true_type {};
 
 template <class T, typename Fake = typename std::enable_if<std::is_integral<T>::value>::type>
 void print_ip(T ip) {
@@ -17,14 +28,16 @@ void print_ip(const std::string &ip) {
     std::cout << ip << std::endl;
 }
 
-//template <class T,
-//        typename Fake = typename std::enable_if<std::is_same<T, std::vector>>
-//void print_ip(const T &ip) {
-//    for ( auto x : ip ) {
-//        std::cout << x << ".";
-//    }
-//    std::cout << std::endl;
-//}
+template <class T,
+        typename Fake = typename std::enable_if<is_vector_or_list<T>::value>::type>
+void print_ip(const T &ip) {
+    for ( auto [i, it] = std::tuple(0, ip.cbegin()); it != ip.cend(); ++i, ++it ) {
+        std::cout << *it;
+        if ( i != ip.size() - 1 )
+            std::cout << ".";
+    }
+    std::cout << std::endl;
+}
 
 int main() {
 
@@ -33,7 +46,8 @@ int main() {
     print_ip(int(2130706433));
     print_ip(int(8875824491850138409));
     print_ip("8.8.8.8");
-//    print_ip(std::vector<int>{1,2,3,4});
+    print_ip(std::vector<int>{1,2,3,4});
+    print_ip(std::list<int>{10,20,30,40});
 
     return 0;
 }
