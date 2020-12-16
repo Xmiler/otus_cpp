a477413b37cf4c23b9cf165f66621e8ef12edad7
Alexander Uldin <alex.uldin@gmail.com>
2020-12-16 09:14:30 +0300

    Revert "remove all files for review"
    This reverts commit 060b44e9278e8e462e95dafaead732506c5297b2.

---
+++ lib/filter.hpp
@@ -0,0 +1,7 @@
+#pragma ones
+
+#include "types.hpp"
+
+ips_t filter(const ips_t &, const byte_t &);
+ips_t filter(const ips_t &, const byte_t &, const byte_t &);
+ips_t filter_any(const ips_t &, const byte_t &);
---
+++ lib/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <string>
+
+using byte_t = std::uint8_t;
+using ip_t = std::array<byte_t, 4>;
+using ips_t = std::vector<ip_t>;
