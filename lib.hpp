b674e94e35c05d7e6f2fc1ad5123394f56e08c1c
Alexander Uldin <alex.uldin@gmail.com>
2020-12-20 20:31:35 +0300

    exclude type bool from integer case

--- lib.hpp
+++ lib.hpp
@@ -14,7 +14,7 @@ template <typename T>
 struct is_vector_or_list<std::list<T>> : std::true_type {};
 
 template <typename T,
-        typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
+          typename std::enable_if<std::is_integral<T>::value && !std::is_same<T, bool>::value>::type* = nullptr>
 std::string get_ip(const T &ip) {
     std::string out;
     const auto *ptr = reinterpret_cast<const uint8_t *>(&ip);
@@ -27,13 +27,13 @@ std::string get_ip(const T &ip) {
 }
 
 template <typename T,
-        typename std::enable_if<std::is_same<T, std::string>::value>::type* = nullptr>
+          typename std::enable_if<std::is_same<T, std::string>::value>::type* = nullptr>
 std::string get_ip(const T &ip) {
     return ip;
 }
 
 template <typename T,
-        typename std::enable_if<is_vector_or_list<T>::value>::type* = nullptr>
+          typename std::enable_if<is_vector_or_list<T>::value>::type* = nullptr>
 std::string get_ip(const T &ip) {
     std::string out;
     for ( auto [i, it] = std::tuple(0, ip.cbegin()); it != ip.cend(); ++i, ++it ) {
