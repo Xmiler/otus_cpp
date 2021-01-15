2521715d4377e5b0e082a5e962a725ae5cfc26c6
Alexander Uldin <alex.uldin@gmail.com>
2021-01-15 20:57:16 +0300

    restructure project

---
+++ lib/handler.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <memory>
+#include <string>
+#include "handler.h"
+#include "logger.h"
+
+Handler::Handler(int n) : m_n(n), m_nesting(0) {}
+
+void Handler::process(std::stringstream& input) {
+    std::string cmd;
+    while(std::getline(input, cmd)) {
+        process_cmd(cmd);
+    }
+}
+
+void Handler::subscribe(ILogger* logger_ptr) {
+    m_loggers.push_back(logger_ptr);
+}
+
+void Handler::collect(const std::string &cmd) {
+    m_bulk.push_back(cmd);
+}
+
+void Handler::release() {
+    if (m_bulk.empty())
+        return;
+
+    std::stringstream output;
+    std::copy(m_bulk.begin(), std::prev(m_bulk.end()), std::ostream_iterator<std::string>(output, ", "));
+    output << m_bulk.back() << std::endl;
+    auto output_str = output.str();
+
+    m_bulk.clear();
+
+    for (const auto& logger : m_loggers )
+        logger->report(output_str);
+}
+
+void Handler::process_cmd(const std::string& cmd) {
+    if (cmd == "{") {
+        m_nesting++;
+        if (m_nesting == 1)
+            release();
+    } else if (cmd == "}") {
+        m_nesting--;
+        if (m_nesting == 0)
+            release();
+    } else {
+        collect(cmd);
+        if (m_nesting == 0 && m_bulk.size() == m_n)
+            release();
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "lib/logger.h"
 #include "lib/handler.h"
 
@@ -5,8 +6,11 @@
 int main() {
     
     Handler handler(3);
-    handler.subscribe(std::make_shared<ConsoleLogger>());
-    handler.subscribe(std::make_shared<FileLogger>("log"));
+    ConsoleLogger console_logger;
+    FileLogger file_logger("log");
+
+    handler.subscribe(&console_logger);
+    handler.subscribe(&file_logger);
 
     std::stringstream input("cmd1\ncmd2\n{\ncmd3\ncmd4\n}\n{\ncmd5\ncmd6\n{\ncmd7\ncmd8\n}\ncmd9\n}\n{\ncmd10\ncmd11\n");
     handler.process(input);
